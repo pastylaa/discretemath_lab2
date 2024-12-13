d282b428744ad44f82e4e5165e363e7775f7de7c
Pasternak <yuliya.pasternak.2007@gmail.com>
2024-12-13 20:04:29 +0200

    add radius

--- lab.py
+++ lab.py
@@ -1,5 +1,5 @@
 """
-Lab 2 template
+Lab 2. Mandryk Sophia and Pasternak Yuliia
 """
 
 def read_incidence_matrix(filename: str) -> list[list]:
@@ -28,6 +28,7 @@ def read_incidence_matrix(filename: str) -> list[list]:
         incidence_matrix[end][edge_index] = -1
 
     return incidence_matrix
+
 
 def read_adjacency_matrix(filename: str) -> list[list]:
     """
@@ -63,6 +64,7 @@ def read_adjacency_dict(filename: str) -> dict[int, list[int]]:
     :returns dict: the adjacency dict of a given graph
     """
     dictofgraph = {}
+
     with open(filename, 'r', encoding='utf-8') as file:
         file.readline()
         for line in file:
@@ -95,8 +97,8 @@ def iterative_adjacency_dict_dfs(graph: dict[int, list[int]], start: int) -> lis
             visited.append(vertex)  # додаємо вершину до списку відвіданих
             # додаємо суміжні вершини в зворотному порядку, щоб зберегти порядок обходу
             nodes_to_visit.extend(reversed(graph[vertex]))
-    
-#print(iterative_adjacency_dict_dfs({0: [1, 2], 1: [0, 2, 3], 2: [0, 1], 3: []}, 0))
+
+    return visited
 
 
 def iterative_adjacency_matrix_dfs(graph: list[list], start: int) ->list[int]:
@@ -116,17 +118,20 @@ def iterative_adjacency_matrix_dfs(graph: list[list], start: int) ->list[int]:
         vertex = nodes_to_visit.pop()  # вибираємо останню вершину зі стека
         if vertex not in visited:  # якщо вершина ще не відвідана
             visited.append(vertex)  # додаємо її до списку відвіданих
-            for neighbor in range(len(graph[vertex])): # додаємо суміжні вершини в стек в зворотньому порядку
-                if graph[vertex][len(graph[vertex]) - 1 - neighbor] == 1:  # перевірка на зворотний порядок
-                    if (len(graph[vertex]) - 1 - neighbor) not in visited:  # якщо сусід ще не відвіданий
-                        nodes_to_visit.append(len(graph[vertex]) - 1 - neighbor)  # додаємо його в стек
 
+             # додаємо суміжні вершини в стек в зворотньому порядку >>>
+            for neighbor in range(len(graph[vertex])):
+                  # перевірка на зворотний порядок >>>
+                if graph[vertex][len(graph[vertex]) - 1 - neighbor] == 1:
+                      # якщо сусід ще не відвіданий >>
+                    if (len(graph[vertex]) - 1 - neighbor) not in visited:
+                          # додаємо його в стек >>>
+                        nodes_to_visit.append(len(graph[vertex]) - 1 - neighbor)
     return visited
-#print(iterative_adjacency_matrix_dfs([[0, 1, 1, 0], [1, 0, 1, 1], [1, 1, 0, 0], [0, 0, 0, 0]], 0))
 
 
-
-def recursive_adjacency_dict_dfs(graph: dict[int, list[int]], start: int, visited=None) -> list[int]:
+def recursive_adjacency_dict_dfs(graph: dict[int, list[int]], start: int,\
+                                 visited=None) -> list[int]:
     """
     :param list[list] graph: the adjacency list of a given graph
     :param int start: start vertex of search
@@ -137,14 +142,16 @@ def recursive_adjacency_dict_dfs(graph: dict[int, list[int]], start: int, visite
     [0, 1, 2, 3]
     """
     if visited is None: #робимо список вершин якщо його нема
-        visited = [] 
+        visited = []
     visited.append(start)#додаємо поточну вершину до списку відвіданих
-    for neighbor in graph[start]: #для кожного сусіда поточної вершини перевіряємо, чи він ще не відвіданий
-        if neighbor not in visited:
-            recursive_adjacency_dict_dfs(graph, neighbor, visited) #викликаємо функцію для не відвіданого сусіда рекурсивно
-    return visited
-# print(recursive_adjacency_dict_dfs({0: [1, 2], 1: [0, 2], 2: [0, 1]}, 0))
 
+    #для кожного сусіда поточної вершини перевіряємо, чи він ще не відвіданий >>>
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            #викликаємо функцію для не відвіданого сусіда рекурсивно >>>
+            recursive_adjacency_dict_dfs(graph, neighbor, visited)
+
+    return visited
 
 
 def iterative_adjacency_dict_bfs(graph: dict[int, list[int]], start: int) -> list[int]:
@@ -159,16 +166,18 @@ def iterative_adjacency_dict_bfs(graph: dict[int, list[int]], start: int) -> lis
     """
     visited = []
     next_to_visit = [start] #робимо стек з початковою вершиною
+
     while next_to_visit: # ітеруємося поки черга не пуста
         vertex = next_to_visit.pop(0) #першу вершину з черги
         if vertex not in visited: #якщо вершинка ще не відвідана, додаємо її до списку відвіданих
             visited.append(vertex)
-            for neighbour in graph[vertex]: # додаю до черги всіх сусідів, яких ще не відвідали і які не знаходяться в черзі
-                 if neighbour not in visited and neighbour not in next_to_visit:
+            for neighbour in graph[vertex]:
+                # додаю до черги всіх сусідів, яких ще не відвідали і які не знаходяться в черзі >>>
+                if neighbour not in visited and neighbour not in next_to_visit:
                     next_to_visit.append(neighbour)
-            
+
     return visited
-#print(iterative_adjacency_dict_bfs({0: [1, 2], 1: [0, 2], 2: [0, 1]}, 0))
+
 
 def iterative_adjacency_matrix_bfs(graph: list[list[int]], start: int) ->list[int]:
     """
@@ -182,19 +191,21 @@ def iterative_adjacency_matrix_bfs(graph: list[list[int]], start: int) ->list[in
     """
     visited = []
     next_to_visit = [start]
+
     while next_to_visit:
         vertex = next_to_visit.pop(0)
         if vertex not in visited:
             visited.append(vertex)
-            for i, connected in enumerate(graph[vertex]): #додаємо до черги всіх сусідів (індекси сусідів), які суміжні з поточною вершиною
+            for i, connected in enumerate(graph[vertex]):
+                #додаємо до черги всіх сусідів (індекси сусідів), які суміжні з поточною вершиною >>>
                 if connected == 1 and i not in visited and i not in next_to_visit:
                     next_to_visit.append(i)
+
     return visited
-# print(iterative_adjacency_matrix_bfs([[0, 1, 1], [1, 0, 1], [1, 1, 0]], 0))
 
 
-
-def recursive_adjacency_matrix_bfs(graph: list[list[int]], start: int, visited=None, next_to_visit=None) ->list[int]:
+def recursive_adjacency_matrix_bfs(graph: list[list[int]], start: int,\
+                                   visited=None, next_to_visit=None) ->list[int]:
     """
     :param dict graph: the adjacency matrix of a given graph
     :param int start: start vertex of search
@@ -213,11 +224,11 @@ def recursive_adjacency_matrix_bfs(graph: list[list[int]], start: int, visited=N
     vertex = next_to_visit.pop(0)
     if vertex not in visited:
         visited.append(vertex)
-        neighbors = [i for i, is_connected in enumerate(graph[vertex]) if is_connected == 1 and i not in visited]
+        neighbors = [i for i, is_connected in enumerate(graph[vertex])\
+                     if is_connected == 1 and i not in visited]
         next_to_visit.extend(neighbors)
 
     return recursive_adjacency_matrix_bfs(graph, start, visited, next_to_visit)
-# print(recursive_adjacency_matrix_bfs([[0, 1, 1], [1, 0, 1], [1, 1, 0]], 0))
 
 
 def adjacency_matrix_radius(graph: list[list]) -> int:
@@ -226,11 +237,37 @@ def adjacency_matrix_radius(graph: list[list]) -> int:
     :returns int: the radius of the graph
     >>> adjacency_matrix_radius([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
     1
-    >>> adjacency_matrix_radius([[0, 1, 1], [1, 0, 1], [1, 1, 0], [0, 1, 0]])
-    2
     """
-    pass
+    dictograph = {}
 
+    for i in range(len(graph)):
+        dictograph[i] = []
+    for index1,el1 in enumerate(graph):
+        for index2,_ in enumerate(el1):
+            if el1[index2]:
+                dictograph[index1].append(index2)
+
+    # роблю список ексцентриситет, оскільки радіус - це наймешний з ексцентриситетів,
+    # і я буду обирати зі списку >>>
+    eccentricities = []
+
+    for start in dictograph:
+        # відстані до інших вершин >>>
+        distances = {node: float('inf') for node in dictograph}
+        distances[start] = 0
+        # bfs
+        queue = [start]
+
+        while queue:
+            current = queue.pop(0)
+            for neighbour in dictograph[current]:
+                if distances[neighbour] == float('inf'):
+                    # якщо ще не відвідано вершинку >>>
+                    distances[neighbour] = distances[current] + 1
+                    queue.append(neighbour)
+        eccentricities.append(max(distances.values()))
+
+    return min(eccentricities)
 
 def adjacency_dict_radius(graph: dict[int: list[int]]) -> int:
     """
@@ -241,9 +278,34 @@ def adjacency_dict_radius(graph: dict[int: list[int]]) -> int:
     >>> adjacency_dict_radius({0: [1, 2], 1: [0, 2], 2: [0, 1], 3: [1]})
     2
     """
-    pass
+    if not graph:
+        return 0
+
+    # роблю список ексцентриситет, оскільки радіус - це наймешний з ексцентриситетів,
+    # і я буду обирати зі списку >>>
+    eccentricities = []
+
+    for start in graph:
+        # відстані до інших вершин >>>
+        distances = {node: float('inf') for node in graph}
+        distances[start] = 0
+        # bfs
+        queue = [start]
+
+        while queue:
+            current = queue.pop(0)
+            for neighbour in graph[current]:
+                if distances[neighbour] == float('inf'):
+                    # якщо ще не відвідано вершинку >>>
+                    distances[neighbour] = distances[current] + 1
+                    queue.append(neighbour)
+        eccentricities.append(max(distances.values()))
+
+    return min(eccentricities)
 
 
-# if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod()
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
